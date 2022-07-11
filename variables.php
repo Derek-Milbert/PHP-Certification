efb9fbc2f07ceafc0c1808da7a94b982d948e6eb
Derek Milbert <dmilbert@power-netz.de>
2022-07-11 21:35:15 +0200

    commented out the variable which would cause an error

--- variables.php
+++ variables.php
@@ -23,7 +23,7 @@ $x = 5; // global scope
 
 function myTest1() {
   // using x inside this function will generate an error
-  echo "<p>Variable x inside function is: $x</p>";
+  echo "<p>Variable x inside function is: "/*$x*/." </p>";
 }
 myTest1();
 
