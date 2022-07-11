cc725ef9a6825244a93b332af47a35016fde0882
Derek Milbert <dmilbert@power-netz.de>
2022-07-11 21:38:24 +0200

    Difference between echo and print and how to use them.

---
+++ echo_vs_print.php
@@ -0,0 +1,53 @@
+<?php
+
+/*
+echo and print are more or less the same. They are both used to output data to the screen.
+
+The differences are small: echo has no return value while print has a return value of 1 so it can be used in expressions. echo can take multiple parameters (although such usage is rare) while print can take one argument. echo is marginally faster than print.
+
+The echo statement can be used with or without parentheses: echo or echo().
+
+Display Text
+The following example shows how to output text with the echo command (notice that the text can contain HTML markup):
+*/
+
+echo "<h2>PHP is Fun!</h2>";
+echo "Hello world!<br>";
+echo "I'm about to learn PHP!<br>";
+echo "This ", "string ", "was ", "made ", "with multiple parameters.";
+
+// Display Variables with echo
+
+$txt1 = "Learn PHP";
+$txt2 = "W3Schools.com";
+$x = 5;
+$y = 4;
+
+echo "<h2>" . $txt1 . "</h2>";
+echo "Study PHP at " . $txt2 . "<br>";
+echo $x + $y;
+
+/*
+The print statement can be used with or without parentheses: print or print().
+
+Display Text
+
+The following example shows how to output text with the print command (notice that the text can contain HTML markup):
+*/
+
+print "<h2>PHP is Fun!</h2>";
+print "Hello world!<br>";
+print "I'm about to learn PHP!";
+
+// Display Variables with print
+
+$txt1 = "Learn PHP";
+$txt2 = "W3Schools.com";
+$x = 5;
+$y = 4;
+
+print "<h2>" . $txt1 . "</h2>";
+print "Study PHP at " . $txt2 . "<br>";
+print $x + $y;
+
+?>
