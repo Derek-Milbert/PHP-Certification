9bf5ac45724446e34c139b69a70df1c084a7b7da
Derek Milbert <dmilbert@power-netz.de>
2022-07-13 08:11:33 +0200

    if else and elseif conditional statements explained

---
+++ Basics/if_else_elseif.php
@@ -0,0 +1,75 @@
+<?php
+
+/*
+PHP Conditional Statements
+Very often when you write code, you want to perform different actions for different conditions. You can use conditional statements in your code to do this.
+
+In PHP we have the following conditional statements:
+
+if statement - executes some code if one condition is true
+if...else statement - executes some code if a condition is true and another code if that condition is false
+if...elseif...else statement - executes different codes for more than two conditions
+switch statement - selects one of many blocks of code to be executed
+*/
+
+/*
+PHP - The if Statement
+The if statement executes some code if one condition is true.
+
+Syntax
+if (condition) {
+  code to be executed if condition is true;
+}
+*/
+
+$t = date("H");
+
+if ($t < "20") {
+  echo "Have a good day!";
+}
+
+/*
+PHP - The if...else Statement
+The if...else statement executes some code if a condition is true and another code if that condition is false.
+
+Syntax
+if (condition) {
+  code to be executed if condition is true;
+} else {
+  code to be executed if condition is false;
+}
+*/
+
+$t = date("H");
+
+if ($t < "20") {
+  echo "Have a good day!";
+} else {
+  echo "Have a good night!";
+}
+
+/*
+PHP - The if...elseif...else Statement
+The if...elseif...else statement executes different codes for more than two conditions.
+
+Syntax
+if (condition) {
+  code to be executed if this condition is true;
+} elseif (condition) {
+  code to be executed if first condition is false and this condition is true;
+} else {
+  code to be executed if all conditions are false;
+}
+*/
+
+$t = date("H");
+
+if ($t < "10") {
+  echo "Have a good morning!";
+} elseif ($t < "20") {
+  echo "Have a good day!";
+} else {
+  echo "Have a good night!";
+}
+
+?>
