ef8ec6816257b398772683eec668ffed28511dd0
Derek Milbert <dmilbert@power-netz.de>
2022-07-13 08:15:06 +0200

    switch case statements exaplined

---
+++ Basics/switch.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+The PHP switch Statement
+Use the switch statement to select one of many blocks of code to be executed.
+
+Syntax
+switch (n) {
+  case label1:
+    code to be executed if n=label1;
+    break;
+  case label2:
+    code to be executed if n=label2;
+    break;
+  case label3:
+    code to be executed if n=label3;
+    break;
+    ...
+  default:
+    code to be executed if n is different from all labels;
+}
+*/
+
+?>
