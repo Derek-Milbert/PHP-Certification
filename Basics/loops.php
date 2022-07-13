f87ae9a0854cede3d3b33ff00aa128573c2d860d
Derek Milbert <dmilbert@power-netz.de>
2022-07-13 08:28:58 +0200

    add Break and Continue examples

--- Basics/loops.php
+++ Basics/loops.php
@@ -77,4 +77,54 @@ foreach($age as $x => $val) {
   echo "$x = $val<br>";
 }
 
+/*
+PHP Break
+
+The break statement can also be used to jump out of a loop.
+This example jumps out of the loop when x is equal to 4:
+*/
+
+for ($x = 0; $x < 10; $x++) {
+  if ($x == 4) {
+    break;
+  }
+  echo "The number is: $x <br>";
+}
+
+/*
+PHP Continue
+
+The continue statement breaks one iteration (in the loop), if a specified condition occurs, and continues with the next iteration in the loop.
+*/
+
+for ($x = 0; $x < 10; $x++) {
+  if ($x == 4) {
+    continue;
+  }
+  echo "The number is: $x <br>";
+}
+
+// Break and continue in a while loop
+
+$x = 0;
+
+while($x < 10) {
+  if ($x == 4) {
+    break;
+  }
+  echo "The number is: $x <br>";
+  $x++;
+}
+
+$x = 0;
+
+while($x < 10) {
+  if ($x == 4) {
+    $x++;
+    continue;
+  }
+  echo "The number is: $x <br>";
+  $x++;
+}
+
 ?>
